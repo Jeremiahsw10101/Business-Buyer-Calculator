7b569429d2681d45a11f6397667c1f2b9bb44c8f
Jeremiahsw10101 <97208860+Jeremiahsw10101@users.noreply.github.com>
2023-12-13 14:19:28 -0600

    v9

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function calculate() {
     var monthlyExpenses = parseInput(document.getElementById('monthlyExpenses').value);
     var investmentAmount = parseInput(document.getElementById('investmentAmount').value);
     var loanAmount = parseInput(document.getElementById('loanAmount').value);
-    var interestRate = parseInput(document.getElementById('interestRate').value) / 100 / 12; // Annual to monthly rate
-    var loanTerm = parseInput(document.getElementById('loanTerm').value) * 12; // Years to months
+    var interestRate = parseInput(document.getElementById('interestRate').value) / 100 / 12; // Convert annual rate to monthly
+    var loanTerm = parseInput(document.getElementById('loanTerm').value) * 12; // Convert years to months
 
     if (isNaN(monthlyRevenue) || isNaN(monthlyExpenses) || isNaN(investmentAmount) ||
         isNaN(loanAmount) || isNaN(interestRate) || isNaN(loanTerm)) {
@@ -12,30 +12,37 @@ function calculate() {
         return;
     }
 
-    var monthlyOperationIncome = monthlyRevenue - monthlyExpenses;
-    var annualOperationIncome = monthlyOperationIncome * 12;
-    var personalBreakEvenPoint = investmentAmount / monthlyOperationIncome;
-    var monthlyLoanPayment = loanAmount * (interestRate * Math.pow(1 + interestRate, loanTerm)) / (Math.pow(1 + interestRate, loanTerm) - 1);
+    var monthlyProfit = monthlyRevenue - monthlyExpenses;
+    var annualProfit = monthlyProfit * 12;
+    var monthlyLoanPayment = loanAmount * interestRate / (1 - Math.pow(1 + interestRate, -loanTerm));
     var totalLoanCost = monthlyLoanPayment * loanTerm;
     var totalInterestPaid = totalLoanCost - loanAmount;
-    var loanBreakEvenPoint = totalLoanCost / monthlyOperationIncome;
-    var adjustedMonthlyProfit = monthlyOperationIncome - monthlyLoanPayment;
-    var adjustedAnnualProfit = adjustedMonthlyProfit * 12;
+    var principal = loanAmount;
+    var interest = totalInterestPaid;
 
-    var resultHTML = `
-        <p>Income from Operations (Monthly): $${formatNumber(monthlyOperationIncome)}</p>
-        <p>Annual Income from Operations: $${formatNumber(annualOperationIncome)}</p>
-        <p>Break-even Point on Personal Investment (months): ${formatNumber(personalBreakEvenPoint)}</p>
-        <p>Break-even Point vs. Total Loan Cost (months): ${formatNumber(loanBreakEvenPoint)}</p>
-        <p>Loan Payment Per Month: $${formatNumber(monthlyLoanPayment)}</p>
-        <p>Total Cost of Loan: $${formatNumber(totalLoanCost)}</p>
-        <p>Total Interest Paid on Loan: $${formatNumber(totalInterestPaid)}</p>
-        <p>Adjusted Monthly Profit After Loan Payments: $${formatNumber(adjustedMonthlyProfit)}</p>
-        <p>Adjusted Yearly Profits After Loan Payments: $${formatNumber(adjustedAnnualProfit)}</p>
+    document.getElementById('result').innerHTML = `
+        <p>Adjusted Monthly Profit After Loan Payments: ${formatNumber(monthlyProfit - monthlyLoanPayment)}</p>
+        <p>Adjusted Yearly Profits After Loan Payments: ${formatNumber(annualProfit - monthlyLoanPayment * 12)}</p>
     `;
-
-    document.getElementById('result').innerHTML = resultHTML;
     document.getElementById('result').style.display = 'block';
+
+    drawPieChart(principal, interest);
+
+    var totalPayments = monthlyLoanPayment * loanTerm;
+
+// Add the missing outputs
+var resultHTML = `
+    <p>Income from Operations (Monthly): ${formatNumber(monthlyOperationIncome)}</p>
+    <p>Annual Income from Operations: ${formatNumber(annualOperationIncome)}</p>
+    <p>Break-even Point on Personal Investment (months): ${formatNumber(personalBreakEvenPoint)}</p>
+    <p>Break-even Point vs. Total Loan Cost (months): ${formatNumber(loanBreakEvenPoint)}</p>
+    <p>Loan Payment Per Month: ${formatNumber(monthlyLoanPayment)}</p>
+    <p>Total Cost of Loan: ${formatNumber(totalLoanCost)}</p>
+    <p>Total Interest Paid on Loan: ${formatNumber(totalInterestPaid)}</p>
+    <p>Adjusted Monthly Profit After Loan Payments: ${formatNumber(adjustedMonthlyProfit)}</p>
+    <p>Adjusted Yearly Profits After Loan Payments: ${formatNumber(adjustedAnnualProfit)}</p>
+`;
+
 }
 
 function clearForm() {
@@ -45,20 +52,66 @@ function clearForm() {
     document.getElementById('loanAmount').value = '';
     document.getElementById('interestRate').value = '';
     document.getElementById('loanTerm').value = '';
-    document.getElementById('result').innerHTML = '';
     document.getElementById('result').style.display = 'none';
-}
-
-function formatNumber(number) {
-    return number.toFixed(2).replace(/\d(?=(\d{3})+(?!\d))/g, '$&,');
+    var loanChart = Chart.getChart("loanChart"); // Get the chart instance
+    if (loanChart) {
+        loanChart.destroy(); // Destroy the chart instance if it exists
+    }
 }
 
 function parseInput(inputValue) {
     return parseFloat(inputValue.replace(/,/g, ''));
 }
 
+function formatNumber(number) {
+    // This will format the number as currency with two decimal places
+    return number.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+}
+
+
+function drawPieChart(principal, interest) {
+    var ctx = document.getElementById('loanChart').getContext('2d');
+    var loanChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Principal', 'Interest'],
+            datasets: [{
+                data: [principal, interest],
+                backgroundColor: ['#4e73df', '#1cc88a'],
+                hoverBackgroundColor: ['#2e59d9', '#17a673'],
+                hoverBorderColor: "rgba(234, 236, 244, 1)",
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+            },
+            legend: {
+                display: false
+            },
+            cutoutPercentage: 80,
+        },
+    });
+}
+
 document.querySelectorAll('input[type=text]').forEach(input => {
     input.addEventListener('input', function(e) {
         this.value = this.value.replace(/[^0-9\.]/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     });
+});
+
+
+// Add this at the end of your script.js
+document.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        calculate();
+    }
 });
